a68fe8f2b6675e45a6ded480e8a68140637bcdb2
wuuff <wuuuufff@gmail.com>
2017-06-12 07:50:02 -0500

    Updated map generation algorithm
    Map generation now creates rooms with columns and removes certain
    walls from rooms in order to create more interesting room shapes.

--- mapgen.c
+++ mapgen.c
@@ -25,13 +25,50 @@ void mapinit(char map[][MAPSIZE], int width, int height){
 #define VERTICAL 1
 #define MIN_WIDTH 6
 #define MIN_HEIGHT 8
+#define HALL_CHANCE 40
+#define MIN_HALL_WIDTH 8
+#define MIN_HALL_HEIGHT 10
+#define MAX_HALL_WIDTH 18
+#define MAX_HALL_HEIGHT 20
+#define EXTRA_DOOR 10
+#define REM_WALL_CHANCE 25
+#define MAX_REM_WALL 10
 void mapgen(char map[][MAPSIZE], int mapwidth, int mapheight, int startx, int starty, int endx, int endy){
-    int i,j,orientation,position,door,doorcount;
+    int i,j,orientation,position,door,door2,doorcount,hall,colspacex,colspacey,remwall;
     int width = endx-startx;
     int height = endy-starty;
 
     if( width < MIN_WIDTH && height < MIN_HEIGHT ){
         return;
+    }
+
+    //Determine whether to generate a big hall with pillars or not.
+    //The size of the room determines the column spacing for now
+    if( width >= MIN_HALL_WIDTH && height >= MIN_HALL_HEIGHT && width < MAX_HALL_WIDTH && height < MAX_HALL_HEIGHT ){
+        hall = rand()%100;//Percent chance out of 100
+        if( HALL_CHANCE > hall ){
+            //Place room columns as densely as the room supports
+            //IF you can divide the room into columns evenly
+            //but make sure there is greater than one space between columns
+            colspacex = width;
+            if( colspacex%2 != 0 ){
+                colspacex--;
+            }
+            while( colspacex%2 == 0 && colspacex > 4 ) colspacex /= 2;
+            colspacey = height;
+            if( colspacey%4 != 0 )
+            {
+                while( colspacey%4 != 0 ) colspacey++;
+            }
+            while( colspacey%4 == 0 && colspacey > 4 ) colspacey /= 4;
+
+            for( i = startx+colspacex; i < startx + width - 1; i+=colspacex ){
+                for( j = starty+colspacey; j < starty + height - 2; j+=colspacey ){
+                    map[j][i] = 1;
+                }
+            }
+            return;//Do not subdivide; return immediately
+        }
     }
 
     //Determine whether we will split the space
@@ -79,10 +116,57 @@ void mapgen(char map[][MAPSIZE], int mapwidth, int mapheight, int startx, int st
         }
         //Generate a door at a random position
         door = startx + 1 + (rand()%(width-1));
+        //Generate an extra door if wall is long enough.
+        //Doors may overlap or be next to each other because such
+        //doors shouldn't be a problem and it might result in interesting maps.
+        //If it is not long enough set to -1 so it won't interfere
+        door2 = width >= EXTRA_DOOR ? startx + 1 + (rand()%(width-1)) : -1;
         //printf("HORIZ %d\n",position);
-        for( i = startx; i < startx + width; i++ ){
-            if( i != door )
+        for( i = startx+1; i < startx + width; i++ ){
+            if( i != door && i != door2 )
                 map[position][i] = 1;
+        }
+        //Determine whether or not we will remove a wall.
+        //We can't remove bottom and right walls because the regions beyond them
+        //haven't been generated yet, so we only try to remove top and left walls.
+        //If we are not on the map edges
+        if( startx > 0 && endx < mapwidth-1 ){
+            //If the new space is taller than it is wide
+            //and the wall is short enough
+            if( /*position - starty > width &&*/ position - starty < MAX_REM_WALL ){
+                remwall = 1;
+                //Check if there is no wall touching the wall we want to remove.
+                //If not, we can remove the wall
+                for( i = starty+1; i < position; i++ ){
+                    if( map[i][startx-1] != 0 ){
+                        remwall = 0;
+                        break;
+                    }
+                }
+                if( remwall && REM_WALL_CHANCE > (rand()%100) ){
+                    for( i = starty+1; i < position; i++ ){
+                        map[i][startx] = 0;//2;//Clear left side of upper half
+                    }
+                }
+            }
+            //If the new space is taller than it is wide
+            //and the wall is short enough
+            if( /*endy - position > width &&*/ endy - position < MAX_REM_WALL ){
+                remwall = 1;
+                //Check if there is no wall touching the wall we want to remove.
+                //If not, we can remove the wall
+                for( i = position+1; i < endy; i++ ){
+                    if( map[i][startx+1] != 0 ){
+                        remwall = 0;
+                        break;
+                    }
+                }
+                if( remwall && REM_WALL_CHANCE > (rand()%100) ){
+                    for( i = position+1; i < endy; i++ ){
+                        map[i][startx] = 0;//4;//Clear left side of lower half
+                    }
+                }
+            }
         }
         //Recursively call to fill the two new spaces we generated
         mapgen(map, mapwidth, mapheight, startx, starty, endx,position);
@@ -98,10 +182,57 @@ void mapgen(char map[][MAPSIZE], int mapwidth, int mapheight, int startx, int st
         //Generate a door at a random position
         //(allocating space for it to be 2 high)
         door = starty + 1 + (rand()%(height-2));
+        //Generate an extra door if wall is long enough.
+        //Doors may overlap or be next to each other because such
+        //doors shouldn't be a problem and it might result in interesting maps.
+        //If it is not long enough set to -1 so it won't interfere
+        door2 = height >= EXTRA_DOOR ? starty + 1 + (rand()%(height-2)) : -1;
         //printf("VERT %d\n",position);
-        for( i = starty; i < starty + height; i++ ){
-            if( i != door && i != door+1 )
+        for( i = starty+1; i < starty + height; i++ ){
+            if( i != door && i != door+1 && i != door2 && i != door2+1 )
                 map[i][position] = 1;
+        }
+        //Determine whether or not we will remove a wall.
+        //We can't remove bottom and right walls because the regions beyond them
+        //haven't been generated yet, so we only try to remove top and left walls.
+        //If we are not on the map edges
+        if( starty > 0 && endy < mapheight-1 ){
+            //If the new space is wider than it is tall
+            //and the wall is short enough
+            if( /*position - startx > height &&*/ position - startx < MAX_REM_WALL ){
+                remwall = 1;
+                //Check if there is no wall touching the wall we want to remove.
+                //If not, we can remove the wall
+                for( i = startx+1; i < position; i++ ){
+                    if( map[starty-1][i] != 0 ){
+                        remwall = 0;
+                        break;
+                    }
+                }
+                if( remwall && REM_WALL_CHANCE > (rand()%100) ){
+                    for( i = startx+1; i < position; i++ ){
+                        map[starty][i] = 0;//3;//Clear top side of left half
+                    }
+                }
+            }
+            //If the new space is wider than it is tall
+            //and the wall is short enough
+            if( /*endx - position > height &&*/ endx - position < MAX_REM_WALL ){
+                remwall = 1;
+                //Check if there is no wall touching the wall we want to remove.
+                //If not, we can remove the wall
+                for( i = position+1; i < endx; i++ ){
+                    if( map[starty-1][i] != 0 ){
+                        remwall = 0;
+                        break;
+                    }
+                }
+                if( remwall && REM_WALL_CHANCE > (rand()%100) ){
+                    for( i = position+1; i < endx; i++ ){
+                        map[starty][i] = 0;//5;//Clear top side of right half
+                    }
+                }
+            }
         }
         //Recursively call to fill the two new spaces we generated
         mapgen(map, mapwidth, mapheight, startx, starty, position,endy);
